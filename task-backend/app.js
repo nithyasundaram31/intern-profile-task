976e08d963e54965f61e117730667528a81f0ed9
nithya <nithukitty112@gmail.com>
2025-07-11 14:17:25 +0530

    cors change

--- task-backend/app.js
+++ task-backend/app.js
@@ -7,7 +7,7 @@ const cors=require('cors')
 const app=express()
 app.use(express.json());
 app.use(cors({
-      origin: 'https://merry-mousse-39b636.netlify.app', 
+      origin: 'https://jolly-dusk-845d27.netlify.app', 
     credentials: true, // Allow credentials to be sent
 }))
 
