1e8807b24cb5b8e10b73822a15da34bce4816b15
nithya <nithukitty112@gmail.com>
2025-07-12 16:39:53 +0530

    c

--- task-frontend/src/App.jsx
+++ task-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes}  from "react-router-dom"
 import Register from './pages/Register'
 import { ToastContainer } from 'react-toastify'
-import Login from './pages/login'
+import Login from './pages/Login'
 import Profile from './pages/Profile'
 
 function App() {
